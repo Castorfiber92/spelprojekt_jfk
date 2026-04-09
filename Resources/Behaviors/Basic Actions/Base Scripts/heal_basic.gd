bf17d5c864e4b66afa080add4293e16eb649a41a
FabGunn <fabian.gunnarsson@hotmail.com>
2026-04-09 17:59:59 +0200

    Added basic heal functionality and win/lose combat functions.

---
+++ Resources/Behaviors/Basic Actions/Base Scripts/heal_basic.gd
@@ -0,0 +1,22 @@
+extends Behavior
+class_name heal_basic_behavior
+
+func on_execute_action(data):
+	var targets = data as Array[HeroSlot]
+	for target in targets:
+		if target.hero == null: continue
+		var target_hero = target.hero
+		# 1. Create the "Draft" CombatEffect
+		# We set the base damage here.
+		var effect = CombatEffect.new(
+			owner_hero, 
+			target_hero, 
+			"HEAL", 
+			owner_hero.current_damage)
+		
+		# 2. Add tags so other items know what this is
+		# effect.tags.append("basic_attack")
+		# effect.tags.append("melee") # or "ranged" based on your hero
+
+		GameEvents.effect_created.emit(effect)
+		print(owner_hero.hero_data.name, " used ", name, " on ", target_hero.hero_data.name)
